752c70e85aed919d3f4bbfe2649358974d573a3f
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-29 14:47:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # your code goes here
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness, @hygiene = 8
+  end
+
+  def happiness=(happiness)
+    n > 10 ? @happiness = 10 : @happiness
+    n < 0 ? @happiness = 0 : @happiness = n
+    end
+end
