5cc39456628ba38184cd19092d1130a4552723ab
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-29 14:48:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -12,5 +12,10 @@ class Person
   def happiness=(happiness)
     n > 10 ? @happiness = 10 : @happiness
     n < 0 ? @happiness = 0 : @happiness = n
-    end
+  end
+
+  def hygiene=(happiness)
+    n > 10 ? @hygiene = 10 : @hygiene
+    n < 0 ? @hygiene = 0 : @hygiene = n
+  end
 end
