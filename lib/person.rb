773b16b25e780406d08f53f195e26dca88756a59
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-29 15:00:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -56,7 +56,7 @@ class Person
       "blah blah partisan blah lobbyist"
     when "weather"
       self.happiness += 1
-      self.happiness += 1
+      person.happiness += 1
       "blah blah sun blah rain"
     else
       "blah blah blah blah blah"
